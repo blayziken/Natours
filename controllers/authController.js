b0260402105aaf2a65fd186b14089ea121496bc4
blayziken <kingtoluwalope@gmail.com>
2021-06-23 17:51:24 +0100

    added jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,19 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsyncError = require('./../utils/catchAsync');
 
-
 exports.signup = catchAsyncError(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    // const newUser = await User.create(req.body);  Security flaw, anybody can sign up with admin rights\
+
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id },);
+
 
     res.status('201').json({
         status: 'Created',
