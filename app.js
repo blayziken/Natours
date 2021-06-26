b1be96ca01a5274113bd51ca4f70d5cdc9cfc466
blayziken <kingtoluwalope@gmail.com>
2021-06-26 16:12:14 +0100

    api rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,10 +8,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100, // 100 requests per hour
+  windowMs: 60 * 60 * 1000, // Time Window: 1 hr * seconds (60) * milliseconds (1000)
+  message: 'Too many requests from this IP, please try again in an hour'
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
