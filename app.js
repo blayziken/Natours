436bd54f0cc62a6d7d3af36e9d46dc8c676e6dc3
blayziken <kingtoluwalope@gmail.com>
2021-06-26 17:25:05 +0100

    authentication, authorization and security done

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,10 +14,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set Security Http Headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit Requests
 const limiter = rateLimit({
   max: 100, // 100 requests per hour
   windowMs: 60 * 60 * 1000, // Time Window: 1 hr * seconds (60) * milliseconds (1000)
@@ -22,7 +31,22 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
+// Body Parser, reading data from the body into req.body
 app.use(express.json());
+
+// Data sanitiation against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution 
+app.use(hpp({
+  whitelist: ['duration', 'ratingsQuantity', 'maxGroupSize', 'difficulty', 'price']
+  // The Whitelist is simply an array of properties for which we allow dupplicates in the query string
+}));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 
