4f01fd92e7d69471c9ce6b04742294da9a7a5212
blayziken <kingtoluwalope@gmail.com>
2021-06-26 14:40:15 +0100

    updateMe implementation done

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,11 @@ const AppError = require('./../utils/appError');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
+
   Object.keys(obj).forEach(el => {
     if (allowedFields.includes(el)) newObj[el] = obj[el];
   });
+
   return newObj;
 };
 
