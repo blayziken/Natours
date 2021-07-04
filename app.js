cd47e533ac1949ebdb94753fc2dc52eccf595a2f
blayziken <kingtoluwalope@gmail.com>
2021-07-04 20:14:26 +0100

    working with pug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -67,6 +67,8 @@ app.use((req, res, next) => {
 app.use('/', (req, res) => {
   res.status(200).render('base');
 })
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
