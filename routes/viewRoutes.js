6aa44fd4795dab850e43a8d9619cb964e2e4adb4
blayziken <kingtoluwalope@gmail.com>
2021-07-06 19:15:27 +0100

    added more routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,8 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// router.use(authController.isLoggedIn);
+
 // router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
@@ -11,6 +13,8 @@ router.get('/tour/:slug', viewsController.getTour);
 // router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
 // router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/login', viewsController.getLoginForm);
+
 // router.get('/me', authController.protect, viewsController.getAccount);
 
 // router.post(
