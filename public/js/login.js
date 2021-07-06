8323d7c855c9a9a588f10e44e55883c08e1b3b17
blayziken <kingtoluwalope@gmail.com>
2021-07-06 19:14:08 +0100

    added new js files

---
+++ public/js/login.js
@@ -0,0 +1,67 @@
+/* eslint-disable */
+// import axios from 'axios';
+// import { showAlert } from './alerts';
+
+// export const login = async (email, password) => {
+//   try {
+//     const res = await axios({
+//       method: 'POST',
+//       url: 'http://127.0.0.1:8000/api/v1/users/login',
+//       data: {
+//         email,
+//         password
+//       }
+//     });
+
+//     if (res.data.status === 'success') {
+//       showAlert('success', 'Logged in successfully!');
+//       window.setTimeout(() => {
+//         location.assign('/');
+//       }, 1500);
+//     }
+//   } catch (err) {
+//     showAlert('error', err.response.data.message);
+//   }
+// };
+
+// export const logout = async () => {
+//   try {
+//     const res = await axios({
+//       method: 'GET',
+//       url: 'http://127.0.0.1:3000/api/v1/users/logout'
+//     });
+//     if ((res.data.status = 'success')) location.reload(true);
+//   } catch (err) {
+//     console.log(err.response);
+//     showAlert('error', 'Error logging out! Try again.');
+//   }
+// };
+
+const login = async (email, password) => {
+  console.log(email, password);
+  console.log('here');
+
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+    console.log(res);
+  } catch (err) {
+    console.log('here 2');
+    // console.log(err.response.data);
+    console.log(err);
+
+  }
+}
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
