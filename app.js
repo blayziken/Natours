fa88da346a13dbf99110ed5b00ce18ed59677f74
blayziken <kingtoluwalope@gmail.com>
2021-07-06 19:14:55 +0100

    added npm axios

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -45,6 +46,7 @@ app.use('/api', limiter);
 
 // Body Parser, reading data from the body into req.body
 app.use(express.json());
+app.use(cookieParser());
 
 // Data sanitiation against NoSQL query injection
 app.use(mongoSanitize());
