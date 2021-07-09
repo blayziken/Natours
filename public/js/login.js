43f60d4e1c7b2d0bc803660d7a5074a5dcb65b6d
blayziken <kingtoluwalope@gmail.com>
2021-07-09 22:20:36 +0100

    logOut finally done

--- public/js/login.js
+++ public/js/login.js
@@ -3,13 +3,8 @@
 // import { showAlert } from './alerts';
 
 
-// document.querySelector('.nav__el--cta').addEventListener("click", e => {
-//   e.preventDefault();
-//   console.log('Here!!!!!!!!!')
-// });
-
-
-
+var loginSubmitButton = document.querySelector('.form');
+var logOutClick = document.querySelector('.nav__el--logout');
 
 
 // Design Frontend Logic for 'Alert'
@@ -61,22 +56,9 @@ const login = async (email, password) => {
   }
 }
 
-// ONCLICK SUBMIT ON LOGIN FORM
-document.querySelector('.form').addEventListener('submit', e => {
-  console.log('form form');
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
-
 // LOGOUT IMPLEMENTATION
 const logout = async () => {
-  console.log('-111111111111111111111111111111111111')
-
   try {
-    console.log('2222222222222222222222222222222')
-
     const res = await axios({
       method: 'GET',
       url: 'http://127.0.0.1:8000/api/v1/users/logout'
@@ -85,90 +67,27 @@ const logout = async () => {
     if ((res.data.status = 'success')) location.reload(true);
 
   } catch (err) {
-    console.log('-----------------------------------------')
     console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
   }
 };
 
-document.querySelector('.nav__el--logout').addEventListener('click', () => {
-  console.log('BBBBBBBBBBBBBBBB');
-  logout();
-});
 
+// EVENT LISTENERS FOR LOGIN AND LOG OUT
 
-
-// document.querySelector('.nav__el--logout').addEventListener('click', () => {
-
-//   console.log('AAAA');
-// });
-
-
-
-// document.querySelector('.nav__el').addEventListener("click", function (e) {
-//   e.preventDefault();
-
-//   alert("Hello World");
-//   console.log('AAAA');
-// });
-
-
-// document.querySelector('.nav__el.nav__el--logout').addEventListener('click', () => {
-
-//   console.log('FULLLLLLLLLLLLLLLLLLL');
-// });
-
-
-
-// document.querySelector('.nav__user-img').addEventListener('click', () => {
-
-//   console.log('IMAGE!!!!!!!');
-// });
-
-
-
-// document.querySelector('.nav__el--cta').addEventListener('click', () => {
-
-//   console.log('NO USER!!!!!!!!!!!!!');
-// });
-
-// document.querySelector('.header__logo').addEventListener('click', () => {
-
-//   console.log('LOGO................');
-// });
-
-
+// ONCLICK SUBMIT ON LOGIN FORM
+if (loginSubmitButton) {
+  loginSubmitButton.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
 
 // TRIGGER IMPLEMENTATION WHEN LOG OUT BUTTON HIS HIT
-// const logOutBtn = document.querySelector('.nav__el--logout');
-
-// if (logOutBtn) logOutBtn.addEventListener('click', logout);
-
-
-// document.querySelector('.nav__user-img').addEventListener('click', () => {
-//   console.log('AAAAAAAAAAAAAAAA');
-// });
-
-// document.querySelector('.nav__el').addEventListener('click', () => {
-//   console.log('CCCCCCCCCCCCCCC');
-
-// });
-
-
-
-
-
-// document.querySelector('.nav__el').addEventListener('click', function () {
-//   console.log('DDDDDDDDDDDDDD');
-// });
-// a.nav__el.nav__el--logout Log out
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-// document.querySelector('.nav__el--logout').addEventListener("click", logout);
-
-
-
-document.querySelector('.nav__el').addEventListener('click', function () {
-  console.log('DDDDDDDDDDDDDD');
-});
+if (logOutClick) {
+  logOutClick.addEventListener('click', () => {
+    logout();
+  });
+}
