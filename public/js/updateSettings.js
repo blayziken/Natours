7fb2f0dd8cfc844314446aa93d15ddad84b6b9bc
blayziken <kingtoluwalope@gmail.com>
2021-07-10 16:03:25 +0100

    updateData and updatePassword done

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -23,7 +23,7 @@
 //   }
 // };
 
-const updateSettings = async (name, email) => {
+const updateData = async (name, email) => {
   try {
     const res = await axios({
       method: 'PATCH',
@@ -42,4 +42,24 @@ const updateSettings = async (name, email) => {
   }
 };
 
+const updatePassword = async (passwordCurrent, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: 'http://127.0.0.1:8000/api/v1/users/updateMyPassword',
+      data: {
+        passwordCurrent,
+        password,
+        passwordConfirm
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'updated successfully!');
+    }
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err.response.data.message);
+  }
+};
 
