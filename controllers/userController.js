36e6d38536c1371de446f973672c64436ffd7365
blayziken <kingtoluwalope@gmail.com>
2021-07-12 16:25:10 +0100

    changed from disk to buffer storage

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,19 +3,25 @@ const multer = require('multer');
 const catchAsyncError = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerfactory');
+const sharp = require('sharp');
 
 
 //............................................................................................................
 // Image upload using MULTER
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  }
-});
+
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
+
+// Since we're still going to resize the photo, it's best to save it to the memory buffer than the disk storage
+const multerStorage = multer.memoryStorage();
+
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -34,18 +40,16 @@ exports.uploadUserPhoto = upload.single('photo');
 //............................................................................................................
 
 // RESIZE USER PHOTO MIDDLEWARE
-
-
-exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+exports.resizeUserPhoto = catchAsyncError(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  await sharp(req.file.buffer)
+  await sharp(req.file.buffer) // Since the image is stored as a buffer, we can access it like this
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${req.file.filename}`);
+    .toFile(`public/img/users/${req.file.filename}`); // Finally writing to file storage here.
 
   next();
 });
