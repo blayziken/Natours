c90c1a6ee1fc028992235ff7b2899dcb4165d23f
blayziken <kingtoluwalope@gmail.com>
2021-07-12 23:28:38 +0100

    removed comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,9 +41,9 @@ exports.uploadUserPhoto = upload.single('photo');
 
 // RESIZE USER PHOTO MIDDLEWARE
 exports.resizeUserPhoto = catchAsyncError(async (req, res, next) => {
-  console.log('-------------Resize User Photo-------------');
-  console.log(req.file);
-  console.log('--------------------------');
+  // console.log('-------------Resize User Photo-------------');
+  // console.log(req.file);
+  // console.log('--------------------------');
 
   if (!req.file) return next();
 
@@ -89,11 +89,6 @@ exports.deleteUser = factory.deleteOne(User);
 
 exports.updateMe = catchAsyncError(async (req, res, next) => {
   // console.log(req.file);
-  console.log('#####################');
-
-  console.log(req.body.photo);
-  console.log('#####################');
-
 
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
