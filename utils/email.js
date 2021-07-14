e8b5b993ecd78c7eff2c15e1b9ff1c91053d5153
blayziken <kingtoluwalope@gmail.com>
2021-07-14 16:16:42 +0100

    created an email class for different functions

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,72 @@
 const nodemailer = require('nodemailer');
 
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Toluwalope Jesufemi <${process.env.EMAIL_FROM}>`;
+    }
+
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // Sendgrid
+            return nodemailer.createTransport({
+                service: 'SendGrid',
+                auth: {
+                    user: process.env.SENDGRID_USERNAME,
+                    pass: process.env.SENDGRID_PASSWORD
+                }
+            });
+        }
+
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+    }
+
+    // Send the actual email
+    async send(template, subject) {
+        // 1) Render HTML based on a pug template
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        // 2) Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject: subject,
+            html: html,
+            text: htmlToText.fromString(html)
+        };
+
+        // 3) Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome to the Natours Family!');
+    }
+
+    async sendPasswordReset() {
+        await this.send(
+            'passwordReset',
+            'Your password reset token (valid for only 10 minutes)'
+        );
+    }
+};
+
 const sendEmail = async options => {
     // In order to send emails with Nodemailer, follow these 3 steps:
 
@@ -19,7 +86,7 @@ const sendEmail = async options => {
 
     // 2) Define Email Options
     const mailOptions = {
-        from: 'Toluwalope Jesufemi <hello@natour.io>',
+        from: `Toluwalope Jesufemi <${process.env.EMAIL_FROM}>`,
         to: options.email,
         subject: options.subject,
         text: options.message,
