2b6e47c45fcd07d936567f1c52c30732e26887f9
blayziken <kingtoluwalope@gmail.com>
2021-07-19 21:38:10 +0100

    added booking router

--- app.js
+++ app.js
@@ -93,6 +93,8 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
+
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
