55080554726c3bcec96e6656132932ba9a90a34c
blayziken <kingtoluwalope@gmail.com>
2021-07-20 17:54:38 +0100

    added render page for signup

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -36,11 +36,20 @@ exports.getTour = catchAsync(async (req, res, next) => {
     });
 });
 
+// LOGIN PAGE
 exports.getLoginForm = (req, res) => {
     res.status(200).render('login', {
         title: 'Log into your account'
     });
 };
+
+// SIGNUP PAGE
+exports.getSignUpForm = (req, res) => {
+    res.status(200).render('signup', {
+        title: 'Create new account'
+    });
+}
+
 
 exports.getAccount = (req, res) => {
     res.status(200).render('account', {
