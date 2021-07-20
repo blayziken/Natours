c4bd0ba25ca669ec04011c6d0187ad579d59d470
blayziken <kingtoluwalope@gmail.com>
2021-07-20 18:02:37 +0100

    added npm compression

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -82,6 +83,8 @@ app.use(hpp({
   whitelist: ['duration', 'ratingsQuantity', 'maxGroupSize', 'difficulty', 'price']
   // The Whitelist is simply an array of properties for which we allow dupplicates in the query string
 }));
+
+app.use(compression());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
