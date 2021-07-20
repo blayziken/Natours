871e971db9afa957b89eaabb0b42b300de7d4c18
blayziken <kingtoluwalope@gmail.com>
2021-07-20 18:04:10 +0100

    preparing for deployment

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,33 +1,10 @@
 // /* eslint-disable */
 
-
-// type is either 'password' or 'data'
-// const updateSettings = async (data, type) => {
-//   try {
-//     const url =
-//       type === 'password'
-//         ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-//         : 'http://127.0.0.1:3000/api/v1/users/updateMe';
-
-//     const res = await axios({
-//       method: 'PATCH',
-//       url,
-//       data
-//     });
-
-//     if (res.data.status === 'success') {
-//       showAlert('success', `${type.toUpperCase()} updated successfully!`);
-//     }
-//   } catch (err) {
-//     showAlert('error', err.response.data.message);
-//   }
-// };
-
 const updateData = async (form) => {
   try {
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMe',
+      url: '/api/v1/users/updateMe',
       data: form
     });
 
@@ -43,7 +20,7 @@ const updatePassword = async (passwordCurrent, password, passwordConfirm) => {
   try {
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMyPassword',
+      url: '/api/v1/users/updateMyPassword',
       data: {
         passwordCurrent,
         password,
@@ -55,7 +32,6 @@ const updatePassword = async (passwordCurrent, password, passwordConfirm) => {
       showAlert('success', 'updated successfully!');
     }
   } catch (err) {
-    console.log(err);
     showAlert('error', err.response.data.message);
   }
 };
